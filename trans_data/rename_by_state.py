d395bd68affff30e3ec2844295bd0a7ff4debd5b
jaewonE <importjaewonE@gmail.com>
2023-08-06 21:52:06 +0900

    Rename audio files with state name

--- trans_data/rename_by_state.py
+++ trans_data/rename_by_state.py
@@ -1,17 +1,22 @@
 import os
 from typing import Optional
 
+# import sys
+# sys.path.append('/Users/jaewone/developer/tensorflow/baby-cry-classification')
+
 from utils.os import rename_by_keyword, get_state_list_from_dir_name
-from constant.os import *
 
 
-def rename_files_by_state(data_path: str, state_list: Optional[list[str]] = None):
+def rename_files_by_state(data_path: str, state_list: Optional[list[str]] = None, include_etc=False):
     """
     state에 따라 파일의 이름을 변경한다.
 
     Parameters:
         data_path : 파일의 경로
+
         state_list=None : state 리스트를 받을 경우 state_list가 포함하는 state 폴더의 파일들만 이름을 변경한다.
+
+        include_etc=False : True일 경우 etc 폴더를 state에 포함한다.
 
     Returns: None
     """
@@ -21,7 +26,8 @@ def rename_files_by_state(data_path: str, state_list: Optional[list[str]] = None
 
     # Get state list if not exist
     if state_list == None:
-        state_list = get_state_list_from_dir_name(data_path)
+        state_list = get_state_list_from_dir_name(
+            data_path, include_etc=include_etc)
     else:
         for state in state_list:
             if not os.path.exists(os.path.join(data_path, state)):
@@ -37,5 +43,8 @@ def rename_files_by_state(data_path: str, state_list: Optional[list[str]] = None
 
 
 if __name__ == '__main__':
-    data_path = ''
+    from constant.os import *
+
+    data_path = data_path
     rename_files_by_state(data_path)
+    print("Done")
--- utils/os/__init__.py
+++ utils/os/__init__.py
@@ -5,7 +5,7 @@ from .copy import *
 from .rename import *
 
 
-def get_state_list_from_dir_name(data_path: str, with_path=False) -> list[str]:
+def get_state_list_from_dir_name(data_path: str, with_path=False, include_etc=False) -> list[str]:
     """
     폴더 이름으로부터 state list를 추출한다.
 
@@ -13,6 +13,8 @@ def get_state_list_from_dir_name(data_path: str, with_path=False) -> list[str]:
       data_path : state list를 추출할 폴더 경로
 
       with_path=False : True일 경우 state 폴더의 경로를 가져온다.
+
+      include_etc=False : True일 경우 etc 폴더를 state에 포함한다.
 
     Returns:
       state 리스트를 반환한다.
@@ -22,7 +24,8 @@ def get_state_list_from_dir_name(data_path: str, with_path=False) -> list[str]:
         dir_path = os.path.join(data_path, dir)
         if os.path.isdir(dir_path):
             state_list.append(dir_path if with_path else dir)
+
     etc_path = os.path.join(data_path, 'etc')
-    if os.path.exists(etc_path):
+    if include_etc == False and os.path.exists(etc_path):
         state_list.remove(etc_path if with_path else 'etc')
     return state_list
